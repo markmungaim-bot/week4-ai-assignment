e3230f22300cb2daf51b3dc34412864ff54fcbe7
Mark Mungai <markmungai.m@gmail.com>
2026-03-12 15:14:54 +0300

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,1 +1,27 @@
+#Question 4
+print("Hello AI Students")
 
+#Question 5
+name="John"
+age=20
+city="Nairobi"
+
+print(f"Name: {name}")
+print(f"Age: {age}")
+print(f"City: {city}")
+
+#Question 6
+print(languages[0])
+
+#Question 7
+student = {
+  "name": "Mark",
+  "age": 19,
+  "course": "AI"
+}
+
+print(student["name"])
+
+#Bonus Question
+A list is a collection of items stored in order and accessed using index numbers. It is written using square brackets []
+A dictionary stores data in key-value pairs and vaalues are accessed using keys. It is written using curly brackets {}
